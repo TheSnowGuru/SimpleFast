30394313492e6268fa4a9bb8a6c77761c80b0667
Shay Ronen <shayronen@gmail.com>
2023-05-16 11:00:58 +0300

    Create compiler.py

---
+++ src/compiler.py
@@ -0,0 +1,5 @@
+#This file contains the Compiler class that compiles the generated C++ code into an executable binary.
+class Compiler:
+    def compile(self, cpp_code):
+        # Implementation of compilation
+        pass
