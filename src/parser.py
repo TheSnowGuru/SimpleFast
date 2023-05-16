2685b2bbab042b51b5806f97ff2e9f2b60ce84ed
Shay Ronen <shayronen@gmail.com>
2023-05-16 11:00:16 +0300

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,4 @@
+#This file contains the Parser class that checks if the tokens follow the grammar rules of SimpleFast and generates an abstract syntax tree (AST).
 class Parser:
     def parse(self, tokens):
         # Implementation of parsing
