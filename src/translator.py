cf1d2b6bf64182cd702571e4388a61bf81a57054
Shay Ronen <shayronen@gmail.com>
2023-05-16 10:59:32 +0300

    Create translator.py

---
+++ src/translator.py
@@ -0,0 +1,5 @@
+#This file contains the Translator class that walks through the AST and generates equivalent C++ code.
+class Translator:
+    def translate(self, ast):
+        # Implementation of translation
+        pass
